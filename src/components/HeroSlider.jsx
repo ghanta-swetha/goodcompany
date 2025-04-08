a88c4eb6ab9ac6063f67fba6c277e399aed16c05
swetha <ghantaswetha2808@gmail.com>
2025-04-08 18:16:24 +0530

    Your commit messages

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -8,14 +8,14 @@ const HeroSlider = () => {
 
   const slides = [
     {
-      image: "/src/assets/first -image.png",
+      image: "/assets/first -image.png",
       title: "Natural Products for Your Wellbeing",
       description: "Discover our range of organic and natural products",
       buttonText: "Shop Now",
       buttonLink: "/shop",
     },
     {
-      image: "/src/assets/first -image.png",
+      image: "/assets/first -image.png",
       title: "Pure & Organic Ingredients",
       description: "Carefully sourced ingredients for your health",
       buttonText: "Explore",
